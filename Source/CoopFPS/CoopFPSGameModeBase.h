b2cff983483ebdb2250dc67ea264aaf63e7aa41d
Guillaume Almyre <zazarlaliche@gmail.com>
2018-02-21 14:49:09 +0100

    Initial commit

---
+++ Source/CoopFPS/CoopFPSGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPFPS_API ACoopFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
